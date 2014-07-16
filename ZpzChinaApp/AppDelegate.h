c52399697702cade6b1f2a8cff349eb4b341b650
wangy322@sina.com <wangyang@wangyangdeair.lan>
2014-07-16 16:15:21 +0800

    Initial Commit

---
+++ ZpzChinaApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZpzChinaApp
+//
+//  Created by 汪洋 on 14-7-16.
+//  Copyright (c) 2014年 zpzchina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZpzChinaApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZpzChinaApp
+//
+//  Created by 汪洋 on 14-7-16.
+//  Copyright (c) 2014年 zpzchina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
